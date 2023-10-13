1f77d83d4589c20d99eb5e987b95dc10050f09c3
bishalcpgn <85829164+bishalcpgn@users.noreply.github.com>
2023-10-13 12:44:55 +0545

    class template

--- Class Template.cpp
+++ Class Template.cpp
@@ -1,6 +1,6 @@
 //Add two numbers of different data type using class template.
 #include<iostream>
-template<class T1, class T2>
+template<class T1, class T2> 
 float add(T1 a , T2 b)
 {
     return a+b;
